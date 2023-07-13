23fbba5bf58ea129f1dc84857e8334ce87045f82
Lucas Palma <lucaspalma44@gmail.com>
2023-07-13 14:49:45 -0300

    all4

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "bucketlucaspalma-terraform3"
+    bucket = "bucketlucaspalma-terraform12"
     key    = "aws-vm/terraform.tfstate"
     region = "us-east-1"
   }
